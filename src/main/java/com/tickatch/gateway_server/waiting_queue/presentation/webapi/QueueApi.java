5aaad1b263360c4884210523b4fa3ecc15b0f7fe
강동현 <peterkang98@gmail.com>
2025-12-11 16:13:56 +0900

    feat: 토큰 무효화 경로 추가, 대기열 토큰 헤더 제거
    See also: #3

--- src/main/java/com/tickatch/gateway_server/waiting_queue/presentation/webapi/QueueApi.java
+++ src/main/java/com/tickatch/gateway_server/waiting_queue/presentation/webapi/QueueApi.java
@@ -5,6 +5,7 @@ import com.tickatch.gateway_server.waiting_queue.application.WaitingQueueService
 import com.tickatch.gateway_server.waiting_queue.application.dto.QueueStatusResponse;
 import com.tickatch.gateway_server.waiting_queue.presentation.dto.LineUpResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,27 +22,33 @@ public class QueueApi {
   private final WaitingQueueService queueService;
 
   @PostMapping("/lineup")
-  public Mono<ApiResponse<LineUpResponse>> lineUp(@RequestHeader("X-User-Id") String queueUserId) {
-    return queueService.lineUp(queueUserId)
-        .map(res -> ApiResponse.success(new LineUpResponse(res.queueToken()), res.msg()));
+  public Mono<ApiResponse<Void>> lineUp(@RequestHeader("X-User-Id") String userId) {
+    return queueService.lineUp(userId)
+        .map(msg -> ApiResponse.success(null, msg));
   }
 
   @GetMapping("/status")
-  public Mono<ApiResponse<QueueStatusResponse>> status(
-      @RequestHeader("X-User-Id") String queueUserId,
-      @RequestHeader("X-Queue-Token") String queueToken
-  ) {
-    return queueService.canEnter(queueToken, queueUserId)
+  public Mono<ApiResponse<QueueStatusResponse>> status(@RequestHeader("X-User-Id") String userId) {
+    return queueService.canEnter(userId)
         .flatMap(canEnter -> {
           if (canEnter) {
             return Mono.just(ApiResponse.success(null, "입장 가능합니다."));
           } else {
-            return queueService.getStatus(queueToken)
+            return queueService.getStatus(userId)
                 .flatMap(status -> Mono.just(ApiResponse.success(status)));
           }
         });
   }
 
-//  @DeleteMapping("/token")
-//  public Mono<ApiResponse<Void>> removeToken()
+  @DeleteMapping
+  public Mono<ApiResponse<Void>> removeToken(@RequestHeader("X-User-Id") String userId) {
+    return queueService.removeAllowedToken(userId)
+        .map(removed -> {
+          if (removed) {
+            return ApiResponse.success(null, "입장 토큰이 무효화되었습니다.");
+          } else {
+            return ApiResponse.error("NOT_FOUND", "무효화할 토큰이 없습니다.", HttpStatus.NOT_FOUND.value());
+          }
+        });
+  }
 }
