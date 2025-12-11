12fd16b1464bc29b78d81eb77cb6244d501b3ecc
강동현 <peterkang98@gmail.com>
2025-12-11 16:14:48 +0900

    feat: 10초마다 만료된 입장 토큰을 삭제하도록 스케줄러 코드를 작성
    See also: #3

--- src/main/java/com/tickatch/gateway_server/waiting_queue/infrastructure/scheduler/QueueScheduler.java
+++ src/main/java/com/tickatch/gateway_server/waiting_queue/infrastructure/scheduler/QueueScheduler.java
@@ -2,17 +2,26 @@ package com.tickatch.gateway_server.waiting_queue.infrastructure.scheduler;
 
 import com.tickatch.gateway_server.waiting_queue.application.WaitingQueueService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class QueueScheduler {
 
   private final WaitingQueueService queueService;
 
-  @Scheduled(fixedRate = 5000)
+  @Scheduled(fixedRate = 10000, initialDelay = 10000)
   public void processNextEntry() {
-//    queueService.admitNextPerson().subscribe();
+    queueService.cleanupExpiredTokens()
+        .doOnSuccess(v -> log.info("만료 토큰 정리 완료"))
+        .onErrorResume(error -> {
+          log.error("만료 토큰 정리 중 오류 발생", error);
+          return Mono.empty();  // 에러를 삼켜서 다음 스케줄은 정상 실행되도록
+        })
+        .subscribe();
   }
 }
