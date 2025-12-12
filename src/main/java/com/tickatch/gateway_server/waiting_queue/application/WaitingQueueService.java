1c9bf29d56e885fae8517de532a29951db64b0ad
강동현 <peterkang98@gmail.com>
2025-12-12 12:01:32 +0900

    fix: 대기열 서비스 수정
    기존에는 입장 가능 인원 수 확인 메소드와 실제 입장을 처리하는 메소드가 분리 되어있어서 race condition이 발생했는데, 이제는 하나의 스크립트로 묶어서 불필요한 메소드들을 삭제
    
    See also: #7

--- src/main/java/com/tickatch/gateway_server/waiting_queue/application/WaitingQueueService.java
+++ src/main/java/com/tickatch/gateway_server/waiting_queue/application/WaitingQueueService.java
@@ -3,7 +3,6 @@ package com.tickatch.gateway_server.waiting_queue.application;
 import com.tickatch.gateway_server.global.util.HmacUtil;
 import com.tickatch.gateway_server.waiting_queue.application.dto.QueueStatusResponse;
 import com.tickatch.gateway_server.waiting_queue.application.port.QueueRepository;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -23,8 +22,7 @@ public class WaitingQueueService {
 
   public Mono<String> lineUp(String userId) {
     String token = getQueueToken(userId);
-    return checkAlreadyAllowedIn(token)
-        .switchIfEmpty(enqueuedNewUser(token));
+    return queueRepository.lineUp(token);
   }
 
   public Mono<Boolean> canEnter(String userId) {
@@ -55,16 +53,6 @@ public class WaitingQueueService {
     return queueRepository.cleanupExpiredTokens()
         .flatMapMany(i -> admitNextPerson())
         .then();
-  }
-
-  private Mono<String> checkAlreadyAllowedIn(String token) {
-    return queueRepository.isAlreadyAllowedIn(token)
-        .filter(Boolean::booleanValue) // False면 자동으로 Mono.empty()를 반환
-        .map(isAllowed -> "이미 입장 가능한 상태입니다.");
-  }
-
-  private Mono<String> enqueuedNewUser(String token) {
-    return queueRepository.lineUp(token).then(Mono.just("대기열에 등록되었습니다."));
   }
 
   private String getQueueToken(String userId) {
